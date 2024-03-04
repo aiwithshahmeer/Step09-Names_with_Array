dff96687853a98ceb13131b8dcd8ae4230f4b439
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-05 03:23:58 +0500

    Add files via upload

---
+++ Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Mr Shahmeer', 'Mr Shahzil', 'Mr Mubashir', 'Mr Meraj', 'Mr Hammad'];
+// for loop:
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] =['Mr Shahmeer', 'Mr Shahzil', 'Mr Mubashir', 'Mr Meraj', 'Mr Hammad'] ;
+
+
+// for loop:
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}
